b145955780ed88568c4095cbba1d82c5661cda03
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-19 17:14:10 -0300

    JPA repository, dependency injection, database seeding

---
+++ course/course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.heitorsaqueto.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.heitorsaqueto.course.entities.User;
+import com.heitorsaqueto.course.repositories.UserRepositiry;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+
+	@Autowired
+	private UserRepositiry userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Dova", "dova@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "HeitorSaqueto", "heitorsaqueto@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+	}
+}
