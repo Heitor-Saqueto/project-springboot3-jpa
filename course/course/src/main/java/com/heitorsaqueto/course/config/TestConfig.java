a2d7256d179eecf8261e431c3d7e40b4deb47e76
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-22 22:06:40 -0300

    Order, Instant, ISO 8601 PART 2

--- course/course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
+++ course/course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.heitorsaqueto.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +8,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.heitorsaqueto.course.entities.Order;
 import com.heitorsaqueto.course.entities.User;
-import com.heitorsaqueto.course.repositories.UserRepositiry;
+import com.heitorsaqueto.course.repositories.OrderRepository;
+import com.heitorsaqueto.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 
 	@Autowired
-	private UserRepositiry userRepository;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,6 +29,11 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Dova", "dova@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "HeitorSaqueto", "heitorsaqueto@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
---
+++ course/course/src/main/java/com/heitorsaqueto/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.heitorsaqueto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.heitorsaqueto.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
