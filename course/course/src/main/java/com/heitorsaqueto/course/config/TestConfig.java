06c599d67a99061010767f754d80416e0ef0b7b8
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-24 12:18:10 -0300

    OrderStatus Enum

--- course/course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
+++ course/course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.heitorsaqueto.course.entities.Order;
 import com.heitorsaqueto.course.entities.User;
+import com.heitorsaqueto.course.entities.enums.OrderStatus;
 import com.heitorsaqueto.course.repositories.OrderRepository;
 import com.heitorsaqueto.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Dova", "dova@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "HeitorSaqueto", "heitorsaqueto@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
