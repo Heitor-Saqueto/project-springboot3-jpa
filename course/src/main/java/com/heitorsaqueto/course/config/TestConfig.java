602c9015c541a8a2ea4a30540785174cb622bcd5
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-28 11:57:53 -0300

    Product

--- course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
+++ course/src/main/java/com/heitorsaqueto/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.heitorsaqueto.course.entities.Category;
 import com.heitorsaqueto.course.entities.Order;
+import com.heitorsaqueto.course.entities.Product;
 import com.heitorsaqueto.course.entities.User;
 import com.heitorsaqueto.course.entities.enums.OrderStatus;
 import com.heitorsaqueto.course.repositories.CategoryRepository;
 import com.heitorsaqueto.course.repositories.OrderRepository;
+import com.heitorsaqueto.course.repositories.ProductRepository;
 import com.heitorsaqueto.course.repositories.UserRepository;
 
 @Configuration
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -44,6 +49,14 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- course/src/main/java/com/heitorsaqueto/course/entities/Category.java
+++ course/src/main/java/com/heitorsaqueto/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.heitorsaqueto.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +23,9 @@ public class Category implements Serializable{
 	
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -42,10 +48,14 @@ public class Category implements Serializable{
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -62,7 +72,4 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-
 }
